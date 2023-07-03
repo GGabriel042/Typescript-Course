9500b14d6d076840d4fe7dce9fbf60d3a7776c23
Gonzagam <canalblacktobio@gmail.com>
2023-07-03 18:08:46 -0300

    Typescript Course - Lesson 1 and 2

---
+++ lesson-2/build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let myName = 'Gabriel';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'Renato';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Hello world';
+album = 1998;
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson-2/src/main.ts
@@ -0,0 +1,23 @@
+let myName: string = 'Gabriel';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = 'Renato';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Hello world';
+album = 1998;
+album = true;
+
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+
+let postId: string | number;
+let isActive: number | boolean;
+
+
+let re: RegExp = /\w+/g
