a06f745b2750f01f68c281c849338c0ecfc34846
Gonzagam <canalblacktobio@gmail.com>
2023-07-04 17:25:16 -0300

    Fixing errors

--- lesson-6/build/js/main.js
+++ lesson-6/build/js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+class Coder {
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
---
+++ lesson-6/src/main.ts
@@ -0,0 +1,24 @@
+class Coder {
+
+  secondLang!: string
+
+  constructor(
+    public readonly name: string, 
+    public music: string,
+    private age: number, 
+    protected lang: string = 'Typescript'
+    ) {
+      this.name = name
+      this.music = music
+      this.age = age
+      this.lang = lang
+    }
+
+  public getAge() {
+    return `Hello, I'm ${this.age}`
+  }
+}
+
+
+const Dave = new Coder('Dave', 'Rock', 42)
+console.log(Dave.getAge())
